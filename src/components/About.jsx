b2635e5801f5a084cdabd3d42d6c208b7a773685
kuslhhh <kushaljadhav7745@gmail.com>
2024-10-01 10:36:35 +0530

    resume button

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,10 @@ const About = () => {
         whileInView={{ opacity: 1, y: 0 }}
         initial={{ opacity: 0, y: -100 }}
         transition={{ duration: 0.5 }}
-      className="my-20 text-center text-4xl">About Me</motion.h2>
+        className="my-20 text-center text-4xl"
+      >
+        About Me
+      </motion.h2>
       <div className="flex flex-wrap">
         <motion.div
           whileInView={{ opacity: 1, x: 0 }}
@@ -31,11 +34,21 @@ const About = () => {
           whileInView={{ opacity: 1, x: 0 }}
           initial={{ opacity: 0, x: 100 }}
           transition={{ duration: 0.5 }}
-          
-        className="w-full lg:w-1/2">
+          className="w-full lg:w-1/2"
+        >
           <div className="flex justify-center lg:justify-start">
             <p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
           </div>
+          <a
+            href="https://drive.google.com/file/d/1T9c6W7_Td5fxsK5O_Oia5Ca3ABg7OjDY/view?usp=sharing"
+            className=" relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
+            <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
+            <span className="relative text-white">Resume</span>
+          </a>
         </motion.div>
       </div>
     </div>
